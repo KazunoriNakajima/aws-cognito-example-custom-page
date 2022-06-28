4e414e74370a2d9c6d5a3100583e681c6704ea67
KazunoriNakajima <kazunori_nakajima_aa@mail.toyota.co.jp>
2022-06-28 22:49:55 +0900

    add package.json

--- aws.js
+++ aws.js
@@ -1,4 +1,5 @@
-export var AWS_REGION = "ap-northeast-1"
-export var AWS_COGNITO_USER_POOL_ID = "xxxxxxxxxxxxxxxxxxxxxxxx"
-export var AWS_COGNITO_APP_CLIENT_ID = "yyyyyyyyyyyyyyyyyyyyyyyyyy"
-export var AWS_COGNITO_ID_POOL_ID = "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"
+export var AWS_REGION = "us-west-1";
+export var AWS_COGNITO_USER_POOL_ID = "us-west-1_TypcBHjXD";
+export var AWS_COGNITO_APP_CLIENT_ID = "2j8sph46neeg7hmflni9537a3h";
+export var AWS_COGNITO_ID_POOL_ID =
+  "arn:aws:cognito-idp:us-west-1:845339839545:userpool/us-west-1_TypcBHjXD";
